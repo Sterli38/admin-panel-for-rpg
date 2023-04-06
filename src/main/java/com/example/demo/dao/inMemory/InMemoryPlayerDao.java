3035188a39644b986e5ae4bc9033c9d7d9ff1211
lenya <lenya.vel@mail.ru>
2023-04-06 23:00:47 +0300

    Добавлена верная фильтрация по name

--- src/main/java/com/example/demo/dao/inMemory/InMemoryPlayerDao.java
+++ src/main/java/com/example/demo/dao/inMemory/InMemoryPlayerDao.java
@@ -9,6 +9,7 @@ import com.example.demo.filter.Filter;
 
 import com.example.demo.filter.PlayerOrder;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -22,59 +23,58 @@ public class InMemoryPlayerDao implements PlayerDao {
     private long idGenerator = 0;
 
     public InMemoryPlayerDao() {
-        Long date = 1679639551000L;
-        Long date1 = 1675580030L;
-        Long date2 = 1676012030L;
-        Player player = new Player();
-        Player player1 = new Player();
-        Player player2 = new Player();
-        player.setName("Egor");
-        player.setTitle("Title");
-        player.setRace(Race.HUMAN);
-        player.setProfession(Profession.WARRIOR);
-        player.setExperience(1000);
-        player.setLevel(5);
-        player.setUntilNextLevel(10);
-        player.setBirthday(date);
-        player.setBanned(true);
-        player1.setName("Lenya");
-        player1.setTitle("Title");
-        player1.setRace(Race.HOBBIT);
-        player1.setProfession(Profession.WARRIOR);
-        player1.setExperience(50000);
-        player1.setLevel(55);
-        player1.setUntilNextLevel(10);
-        player1.setBirthday(date1);
-        player1.setBanned(true);
-        player2.setName("Alex");
-        player2.setTitle("Title");
-        player2.setRace(Race.HUMAN);
-        player2.setProfession(Profession.WARRIOR);
-        player2.setExperience(4000);
-        player2.setLevel(10);
-        player2.setUntilNextLevel(10);
-        player2.setBirthday(date2);
-        player2.setBanned(true);
-        createPlayer(player);
-        createPlayer(player1);
-        createPlayer(player2);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-        createPlayer(player);
-
+//        Long date = 1679639551000L;
+//        Long date1 = 1675580030L;
+//        Long date2 = 1676012030L;
+//        Player player = new Player();
+//        Player player1 = new Player();
+//        Player player2 = new Player();
+//        player.setName("Egor");
+//        player.setTitle("Title");
+//        player.setRace(Race.HUMAN);
+//        player.setProfession(Profession.WARRIOR);
+//        player.setExperience(1000);
+//        player.setLevel(5);
+//        player.setUntilNextLevel(10);
+//        player.setBirthday(date);
+//        player.setBanned(true);
+//        player1.setName("Lenya");
+//        player1.setTitle("Title");
+//        player1.setRace(Race.HOBBIT);
+//        player1.setProfession(Profession.WARRIOR);
+//        player1.setExperience(50000);
+//        player1.setLevel(55);
+//        player1.setUntilNextLevel(10);
+//        player1.setBirthday(date1);
+//        player1.setBanned(false);
+//        player2.setName("Alex");
+//        player2.setTitle("Title");
+//        player2.setRace(Race.HUMAN);
+//        player2.setProfession(Profession.WARRIOR);
+//        player2.setExperience(4000);
+//        player2.setLevel(10);
+//        player2.setUntilNextLevel(10);
+//        player2.setBirthday(date2);
+//        player2.setBanned(false);
+//        createPlayer(player);
+//        createPlayer(player1);
+//        createPlayer(player2);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
+//        createPlayer(player);
     }
 
     public List<Player> getPlayers() {
@@ -123,7 +123,7 @@ public class InMemoryPlayerDao implements PlayerDao {
 
     public List<Player> getPlayersByFilter(Filter filter) {
         return players.values().stream()
-                .filter(player -> filter.getName() == null || player.getName().equals(filter.getName()))
+                .filter(player -> filter.getName() == null || Pattern.compile(Pattern.quote(filter.getName()), Pattern.CASE_INSENSITIVE).matcher(player.getName()).find())
                 .filter(player -> filter.getTitle() == null || player.getTitle().equals(filter.getTitle()))
                 .filter(player -> filter.getRace() == null || player.getRace() == filter.getRace())
                 .filter(player -> filter.getProfession() == null || player.getProfession() == filter.getProfession())
